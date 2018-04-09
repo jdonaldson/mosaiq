54df384637cfe0e57c48937bff5426dfe4089da3
Justin Donaldson <jdonaldson@gmail.com>
2018-04-09 15:39:45 -0700

    add more setup metadata

--- setup.py
+++ setup.py
@@ -3,5 +3,8 @@ from setuptools import setup
 setup(
     name='mosaiq',
     version='0.1',
-    scripts=['mosaiq.py']
+    scripts=['mosaiq.py'],
+    url=['https://github.com/jdonaldson/mosaiq-python'],
+    author=["Justin Donaldson"],
+    author_email=['jdonaldson@gmail.com']
 )
