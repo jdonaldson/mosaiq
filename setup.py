7d53eee43a386789e1c92ea7507464acea84f434
Justin Donaldson <jdonaldson@gmail.com>
2018-04-10 09:05:17 -0700

    license and dependencies

--- setup.py
+++ setup.py
@@ -2,8 +2,10 @@ from setuptools import setup
 
 setup(
     name='mosaiq',
-    version='0.1',
+    version='0.1-1',
     scripts=['mosaiq.py'],
+    install_requires=['pandas', 'matplotlib'],
+    license=['MIT'],
     url=['https://github.com/jdonaldson/mosaiq-python'],
     author=["Justin Donaldson"],
     author_email=['jdonaldson@gmail.com']
