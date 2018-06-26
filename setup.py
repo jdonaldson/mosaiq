8b2376a702894a7bf24a9c18418724353bc5eccd
Justin Donaldson <jdonaldson@gmail.com>
2018-06-26 13:23:21 -0700

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='mosaiq',
-    version='0.1-2',
+    version='0.1-3',
     install_requires=['pandas', 'matplotlib'],
     license=['MIT'],
     url=['https://github.com/jdonaldson/mosaiq-python'],
