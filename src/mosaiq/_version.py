c525a28f344d50b196530cf4076012ac39ec17c4
Justin Donaldson <justin@hushh.ai>
2024-08-30 12:59:28 -0700

    version nit

--- src/mosaiq/_version.py
+++ src/mosaiq/_version.py
@@ -12,5 +12,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '0.1.8.dev0+gfab035c.d20240830'
-__version_tuple__ = version_tuple = (0, 1, 8, 'dev0', 'gfab035c.d20240830')
+__version__ = version = '0.1.8'
+__version_tuple__ = version_tuple = (0, 1, 8)
