7d0f35e7c8d1459ff528d369955cac43897c7cd0
Justin Donaldson <justin@hushh.ai>
2024-08-29 21:02:09 -0700

    add test directory

---
+++ test/test_default.py
@@ -0,0 +1,10 @@
+import mosaiq
+def test_pack():
+    assert mosaiq is not None
+
+def test_top_bin():
+    assert True
+    # dat = pd.DataFrame({x: [10,20,30,40,10,50,60,70]})
+    # result =  mosaiq.top_bin(dat, "x")
+    # import pdb; pdb.set_trace()
+
